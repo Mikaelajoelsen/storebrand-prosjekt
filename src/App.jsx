928435a7fb60a351f09b85aa6eb576fd56572b44
Mikaelajoelsen <111288566+Mikaelajoelsen@users.noreply.github.com>
2024-01-08 22:03:17 +0100

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import Header from "./components/header";
 function App() {
   return (
     <>
-      <Header></Header>
+      <div>
+        <Header />
+      </div>
+      <div></div>
     </>
   );
 }
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,12 +1,202 @@
-import React from "react";
-import { FaBars } from "react-icons/fa6";
+import { useState, Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+const SearchIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+    />
+  </svg>
+);
+
+const navigation = [
+  { name: "Privat", href: "#", current: true },
+  { name: "Bedrift", href: "#", current: false },
+  { name: "Om oss", href: "#", current: false },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const SearchBar = () => {
+  const [isSearchVisible, setIsSearchVisible] = useState(false);
+
+  return (
+    <div
+      className="relative flex items-center"
+      onMouseEnter={() => setIsSearchVisible(true)}
+      onMouseLeave={() => setIsSearchVisible(false)}
+    >
+      <button
+        type="button"
+        className="relative p-1 text-red-900 hover:text-red-900"
+      >
+        {SearchIcon}
+        <span className="absolute -inset-1.5" />
+        <span className="sr-only">Søk.....</span>
+      </button>
+      <input
+        type="text"
+        placeholder="Søk....."
+        className={`w-full py-2 pl-8 pr-4 border border-gray-300 focus:outline-none focus:border-red-900 ${
+          isSearchVisible ? "opacity-100" : "opacity-0 pointer-events-none"
+        } transition-opacity duration-300 ease-in-out`}
+        aria-label="Search"
+      />
+    </div>
+  );
+};
+
 export default function Header() {
   return (
-    <div>
-      <img src="/storebrand-logo.svg" />
-      <div className="nav-icon">
-        <FaBars></FaBars>
-      </div>
-    </div>
+    <Disclosure as="nav">
+      {({ open }) => (
+        <>
+          <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-16">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                <Disclosure.Button className="relative inline-flex items-center justify-center p-2 text-red-900 hover: hover:boder focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block w-6 h-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block w-6 h-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
+                <div className="flex items-center flex-shrink-0">
+                  <img
+                    className="w-1/2"
+                    src="/storebrand-logo.svg"
+                    alt="Logo"
+                  />
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? "text-red-900  hover:bg-red-100 hover:text-red-900 hover:border-t-4 border-red-700"
+                            : "text-red-900 hover:bg-red-100 hover:text-red-900 hover:border-t-4 border-red-700",
+                          "px-3 py-3 text-l font-bold"
+                        )}
+                        aria-current={item.current ? "page" : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <SearchBar />
+
+                <Menu as="div" className="relative ml-3">
+                  <div>
+                    <Menu.Button className="relative flex text-sm bg-gray-800 rounded-full focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <span className="absolute -inset-1.5" />
+                      <span className="sr-only">Open user menu</span>
+                      <img
+                        className="w-8 h-8 rounded-full"
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt="User"
+                      />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 w-48 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active ? "bg-red-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700"
+                            )}
+                          >
+                            Your Profile
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700"
+                            )}
+                          >
+                            Settings
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active ? "bg-red-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700"
+                            )}
+                          >
+                            Sign out
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current
+                      ? "bg-gray-900 text-red-200"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-red-300",
+                    "block rounded-md px-3 py-2 text-base font-medium"
+                  )}
+                  aria-current={item.current ? "page" : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   );
 }
