a38e47f3e9d53676fc0c66b7fd008f59f8d862e7
Mikaelajoelsen <111288566+Mikaelajoelsen@users.noreply.github.com>
2024-01-18 21:17:16 +0100

    changed css in header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -42,7 +42,7 @@ export default function Header() {
                       href={item.href}
                       className={classNames(
                         item.current
-                          ? "bg-gray-900 text-red-900"
+                          ? "bg-gray-900 text-red-800"
                           : "text-red-900 hover:bg-gray-700 hover:text-red-900",
                         "block rounded-md px-3 py-2 text-sm font-medium"
                       )}
